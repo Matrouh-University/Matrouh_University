a73bd1b78d85ecc55a7c44f10d883be70fd574d0
Matrouh-University <167043983+Matrouh-University@users.noreply.github.com>
2024-05-13 01:54:59 +0300

    index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ xhr.onreadystatechange = function() {
                     window.localStorage.setItem("password", JSON.stringify(user.password));
                     window.localStorage.setItem("username", JSON.stringify(user.username));   
                     window.alert(`مرحبا بعودتك يا ${user.name}`);
-                    window.open("mm.html");
+                    window.close();
                     
                     found = true;
                     break;
@@ -30,4 +30,4 @@ xhr.onreadystatechange = function() {
 };
 
 xhr.send();
-window.close();
+
