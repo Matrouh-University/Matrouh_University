814b106ff19eaa09e01497ce4d647570a92f86c3
Matrouh-University <167043983+Matrouh-University@users.noreply.github.com>
2024-05-14 09:45:31 +0300

    index.js

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ xhr.onreadystatechange = function() {
                     window.localStorage.setItem("password", JSON.stringify(user.password));
                     window.localStorage.setItem("username", JSON.stringify(user.username));   
                    //window.alert(`مرحبا بعودتك يا ${user.name}`);
-                   var windows = window.open("https://matrouh-university.github.io/Matrouh_University/mm.htm");
+                    window.open("https://matrouh-university.github.io/Matrouh_University/mm.htm");
+                    window.close("login.html")
 windows.close();
 
 // فتح النافذة الجديدة
